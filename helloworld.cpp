6005188e41066741513d758e73d2b7f6a89b2792
NikitaShim <shim08693@gmail.com>
2023-10-18 12:08:07 +0300

    Commit message

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,6 +4,7 @@
 int
 main()
 {
+<<<<<<< HEAD
     std::string name;
     std::cout<<"Enter your name";// Ввод реального имени
     	std::cin>>
@@ -12,4 +13,13 @@ main()
     	     <<endl;// Вывод вашего реального имени
     return 0;
 
+=======
+  std::string name;
+  std::cout << "Enter your name"; //Ввод реального имени
+      std::cin >> 
+    name;
+  std::cout << "Hello world" << name
+  	    << endl;//Вывод вашего реального имени
+  return 0;
+>>>>>>> ebc5116 (Commit message)
 }
